9a0a9b38369385151cdc7f9d49ad4c58bbc05ece
Lucas Almeida <lucas.almeida@zup.com.br>
2023-09-18 18:00:18 -0300

    new branch qa

--- stackspot-actions/setup-stackspot-workflows/setup.py
+++ stackspot-actions/setup-stackspot-workflows/setup.py
@@ -90,6 +90,7 @@ def __parse_inputs(metadata: Metadata) -> Inputs:
 def run(metadata: Metadata):
     print()
     try:
+        print("hello!")
         inputs = __parse_inputs(metadata)
         provider = PROVIDERS[inputs.provider]
         provider.setup(inputs)
